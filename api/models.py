f3a3a66eeeb9a66d5c79e0bba1a2f361c9235690
Tiberio Z <tzolzett@live.fr>
2025-02-19 14:55:52 +0100

    fix

--- api/models.py
+++ api/models.py
@@ -7,11 +7,6 @@ class FoodRequest(BaseModel):
         title="The barcode of the food item",
         example="123456789",
     )
-    temperature: float = Field(
-        ...,
-        title="The temperature of the food item",
-        example=4.5,
-    )
 
 
 class TemparatureRequest(BaseModel):
